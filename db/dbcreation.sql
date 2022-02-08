7179bc6992be8b54e42ac3f2cdefb15f9eef4ccc
sandersuarez <sandersuarezlara@gmail.com>
2022-02-08 23:06:00 +0100

    Fix some data structure errors and deprecated constraints

--- db/dbcreation.sql
+++ db/dbcreation.sql
@@ -5,68 +5,77 @@ USE adminrps;
 /* Users table creation */
 CREATE TABLE IF NOT EXISTS users
 	(
-	coduser INT UNSIGNED,
+	coduser INT,
     nameuser VARCHAR(60) NOT NULL,
     keyuser VARCHAR(255) NOT NULL,
-    ivaprefuser DECIMAL(3,2) UNSIGNED NOT NULL,
+    ivaprefuser DECIMAL(3,2) NOT NULL,
     datlastkeyuser DATE NOT NULL DEFAULT (CURDATE()),
-    CONSTRAINT pk_users PRIMARY KEY (coduser)
-	) COLLATE 'utf8_bin';
+    CONSTRAINT pk_users PRIMARY KEY (coduser),
+    CONSTRAINT coduser_unsigned CHECK (coduser >= 0),
+    CONSTRAINT ivaprefuser_unsigned CHECK (ivaprefuser >= 0)
+	) COLLATE 'utf8mb4_bin';
 
 /* Products table creation */
 CREATE TABLE IF NOT EXISTS products
 	(
-    codproduct INT UNSIGNED,
-    nameproduct VARCHAR (240) NOT NULL COLLATE 'utf8_spanish_ci',
-    stockproduct INT UNSIGNED NULL DEFAULT NULL,
-    priceproduct DECIMAL(5,2) UNSIGNED NOT NULL,
-    coduser INT UNSIGNED NOT NULL,
+    codproduct INT,
+    nameproduct VARCHAR (240) NOT NULL COLLATE 'utf8mb4_spanish_ci',
+    stockproduct INT NULL DEFAULT NULL,
+    priceproduct DECIMAL(5,2) NOT NULL,
+    coduser INT NOT NULL,
     CONSTRAINT pk_products PRIMARY KEY (codproduct),
+    CONSTRAINT codproduct_unsigned CHECK (codproduct >= 0),
+    CONSTRAINT stockproduct_unsigned CHECK (stockproduct >= 0),
+    CONSTRAINT priceproduct_unsigned CHECK (priceproduct >= 0),
     CONSTRAINT fk_products_users FOREIGN KEY (coduser)
 		REFERENCES users (coduser) ON DELETE CASCADE
         ON UPDATE CASCADE
-    ) COLLATE 'utf8_bin';
+    ) COLLATE 'utf8mb4_bin';
     
 /* Customers table creation */
 CREATE TABLE IF NOT EXISTS customers
 	(
-    codcustomer INT UNSIGNED,
-    namecustomer VARCHAR (60) NOT NULL COLLATE 'utf8_spanish_ci',
+    codcustomer INT,
+    namecustomer VARCHAR (60) NOT NULL COLLATE 'utf8mb4_spanish_ci',
     telcustomer CHAR(9) NOT NULL,
-    coduser INT UNSIGNED NOT NULL,
+    coduser INT NOT NULL,
     CONSTRAINT pk_customers PRIMARY KEY (codcustomer),
+    CONSTRAINT codcustomer_unsigned CHECK (codcustomer >= 0),
     CONSTRAINT fk_customers_users FOREIGN KEY (coduser)
 		REFERENCES users (coduser) ON DELETE CASCADE
         ON UPDATE CASCADE
-    ) COLLATE 'utf8_bin';
+    ) COLLATE 'utf8mb4_bin';
     
 /* Orders table creation */
 CREATE TABLE IF NOT EXISTS orders
 	(
-    codorder INT UNSIGNED,
-    numdayorder SMALLINT UNSIGNED NOT NULL,
+    codorder CHAR(6),
+    numdayorder MEDIUMINT NOT NULL,
     dateorder DATE NOT NULL DEFAULT (CURDATE()),
     hourorder TIME NOT NULL DEFAULT (CURTIME()),
     ordersold BIT NOT NULL DEFAULT 0,
-    moneyreceived DECIMAL(5,2) UNSIGNED NULL,
-    codcustomer INT UNSIGNED NOT NULL,
+    moneyreceived DECIMAL(5,2) NULL DEFAULT NULL,
+    codcustomer INT NOT NULL,
     CONSTRAINT pk_orders PRIMARY KEY (codorder),
+    CONSTRAINT numdayorder_unsigned CHECK (numdayorder > 0),
+    CONSTRAINT moneyreceived_unsigned CHECK (moneyreceived >= 0),
     CONSTRAINT fk_orders_customer FOREIGN KEY (codcustomer)
 		REFERENCES customers (codcustomer) ON DELETE NO ACTION
         ON UPDATE CASCADE
-    ) COLLATE 'utf8_bin';
+    ) COLLATE 'utf8mb4_bin';
 
 /* Contain table creation */
 CREATE TABLE IF NOT EXISTS contain
 	(
-    codproduct INT UNSIGNED,
-    codorder INT UNSIGNED,
-    amountproductorder TINYINT UNSIGNED NOT NULL,
+    codproduct INT,
+    codorder CHAR(6),
+    amountproductorder SMALLINT NOT NULL DEFAULT 1,
     CONSTRAINT pk_contain PRIMARY KEY (codproduct, codorder),
+    CONSTRAINT amountproductorder_restriction CHECK (amountproductorder > 0),
     CONSTRAINT fk_contain_product FOREIGN KEY (codproduct)
 		REFERENCES products (codproduct) ON DELETE NO ACTION
         ON UPDATE CASCADE,
     CONSTRAINT fk_contain_order FOREIGN KEY (codorder)
 		REFERENCES orders (codorder) ON DELETE NO ACTION
         ON UPDATE CASCADE
-    ) COLLATE 'utf8_bin';
+    ) COLLATE 'utf8mb4_bin';
