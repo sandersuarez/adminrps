8b1f42353a26585647b4ad323fa8c5200bb75af2
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-09 17:56:08 +0200

    Added style modules

---
+++ src-js/styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  background: '#FFFFFF',
+  highlight: '#0377BD',
+  menu: '#EEEEEE',
+  extraHighlight: '#DE9C64',
+  section: '#63C8DD',
+  danger: '#915724'
+}
+
+export default colors
---
+++ src-js/styles/fonts.ts
@@ -0,0 +1,9 @@
+import { css } from '@emotion/css'
+
+const fonts = {
+  primary: css({
+    fontFamily: '"Noto Sans", sans-serif',
+  })
+}
+
+export default fonts
