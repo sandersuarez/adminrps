7d7fb8cfe357b0bffe876823b833cfd05290d32a
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-11 17:33:34 +0200

    Global styling moved to a stylesheet

--- src-js/components/Layout.tsx
+++ src-js/components/Layout.tsx
@@ -1,13 +1,10 @@
 import React, { FC, ReactNode } from 'react'
 import Navbar from './Navbar'
 import Sections from '../shapes/Sections'
-import { Global } from '@emotion/react'
-import global from '../styles/global'
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <div>
-      <Global styles={global} />
       <header>
         <h1>AdminRPS</h1>
       </header>
