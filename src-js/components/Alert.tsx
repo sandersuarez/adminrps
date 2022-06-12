a2b2ccf60c25520bbbf2f682ccaa1bb27e47abbe
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-12 15:46:06 +0200

    WIP Alert

--- src-js/components/Alert.tsx
+++ src-js/components/Alert.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import colors from '../styles/colors'
 
-const Alert = () => {
-
-}
+const Container = styled.article({ background: colors.section })
+const Top = styled.div({ background: colors.primary })
 
 export default Alert
