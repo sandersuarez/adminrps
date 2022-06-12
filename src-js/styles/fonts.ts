6bf21d10c7b197fb0ea1af17c29dc12482f58919
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-12 15:45:10 +0200

    Added more fonts

--- src-js/styles/fonts.ts
+++ src-js/styles/fonts.ts
@@ -1,9 +1,35 @@
 import { css } from '@emotion/css'
+import breakpoints from './breakpoints'
 
 const fonts = {
-  primary: css({
-    fontFamily: '"Noto Sans", sans-serif',
+  body: css({
+    fontSize: '1.6rem',
+    [breakpoints.tablet]: {
+      fontSize: '1.9rem',
+    }
+  }),
+  title: css({
+    fontSize: '1.8rem',
+    [breakpoints.tablet]: {
+      fontSize: '2.2rem',
+    }
+  }),
+  subtitle: css({
+    fontSize: '1.8rem',
+    [breakpoints.tablet]: {
+      fontSize: '2rem',
+    }
+  }),
+  orderNumber: css({
+    fontSize: '2.2rem',
+    [breakpoints.tablet]: {
+      fontSize: '3.2rem',
+    }
   })
 }
+
+/*css`
+  ${ fonts.body }
+`*/
 
 export default fonts
