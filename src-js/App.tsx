bbe25d2c7ffbe155eeed7b9a407ea1974b5bfff5
sandersuarez <sandersuarezlara@gmail.com>
2022-06-12 21:24:17 +0200

    Add the emotion global styles

--- src-js/App.tsx
+++ src-js/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Layout from './components/Layout'
 import Home from './components/Home'
+import GlobalStyles from './components/GlobalStyles'
 
 const App = () => {
   return (
-    <Layout>
-      <Home/>
-    </Layout>
+    <>
+      <GlobalStyles />
+      <Layout>
+        <Home />
+      </Layout>
+    </>
+
   )
 }
 
