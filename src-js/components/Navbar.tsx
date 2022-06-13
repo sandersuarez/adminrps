8595721a3be832a2aa47d7fdae49801b42dde92d
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-13 18:07:01 +0200

    Using object styles

--- src-js/components/Navbar.tsx
+++ src-js/components/Navbar.tsx
@@ -1,11 +1,10 @@
-import React, {FC} from 'react'
+import React, { FC } from 'react'
 import Sections from '../shapes/Sections'
 import styled from '@emotion/styled'
-import { css } from '@emotion/css'
 import fonts from '../styles/fonts'
 
 const NavbarStyles = styled.nav(
-  css`${fonts.body}`
+  fonts.body
 )
 
 interface IProps {
