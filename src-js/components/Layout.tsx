f782adecd072c0a60d6dfb3f326fa4a40d16ef82
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-14 15:11:38 +0200

    Switch to object styles

--- src-js/components/Layout.tsx
+++ src-js/components/Layout.tsx
@@ -1,10 +1,16 @@
 import React, { FC, ReactNode } from 'react'
 import Navbar from './Navbar'
 import Sections from '../shapes/Sections'
+import styled from '@emotion/styled'
+import fonts from '../styles/fonts'
+
+const Container = styled.div(
+  fonts.body
+)
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div>
+    <Container>
       <header>
         <h1>AdminRPS</h1>
       </header>
@@ -12,7 +18,7 @@ const Layout: FC<{ children: ReactNode }> = ({ children }) => {
         <Navbar selected={Sections.Products}/>
         {children}
       </main>
-    </div>
+    </Container>
   )
 }
 
