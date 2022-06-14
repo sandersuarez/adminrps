2766648c7377c8d996c8507d26821ed00a94026f
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-14 15:10:55 +0200

    Removed Global on App

--- src-js/App.tsx
+++ src-js/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import Layout from './components/Layout'
 import Home from './components/Home'
-import GlobalStyles from './components/GlobalStyles'
 
 const App = () => {
   return (
-    <>
-      <GlobalStyles />
       <Layout>
         <Home />
       </Layout>
-    </>
-
   )
 }
 
