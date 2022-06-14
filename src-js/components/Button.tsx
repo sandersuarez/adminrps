a2f2e31acad0ffe3f5a578daad2bdaaa52277473
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-14 15:10:37 +0200

    Restructured button

--- src-js/components/Button.tsx
+++ src-js/components/Button.tsx
@@ -2,43 +2,44 @@ import React from 'react'
 import styled from '@emotion/styled'
 import colors from '../styles/colors'
 
-const Base = styled.button({
-  // reset styling
-  border: 'none',
-  font: 'inherit',
-  opacity: 1,
-  appearance: 'none',
-  // custom styling
-  height: '4.5rem',
-  padding: '0 1.7rem',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  borderRadius: '2.2rem',
-  fontWeight: 'bold',
-  whiteSpace: 'nowrap',
-
-  '&[disabled]': {
-    opacity: .6,
-  }
-})
-
-const Button = {
-
-  Secondary: styled(Base)({
-    background: colors.secondary,
-  }),
-
-  Danger: styled(Base)({
-    background: colors.danger,
-    color: '#fff',
+const Button = styled.button<{ customType: 'secondary' | 'danger' }>(
+  {
+    // reset styling
+    border: 'none',
+    font: 'inherit',
+    opacity: 1,
+    appearance: 'none',
+    // custom styling
+    height: '4.5rem',
+    padding: '0 1.7rem',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '2.2rem',
+    fontWeight: 'bold',
+    whiteSpace: 'nowrap',
 
     '&[disabled]': {
-      opacity: .3,
-      color: '#000',
-    }
-  }),
+      opacity: .6,
+    },
+  },
+  ({ customType }) => {
+    switch (customType) {
+      case 'secondary':
+        return { background: colors.secondary, }
 
-}
+      case 'danger':
+        return {
+          background: colors.danger,
+          color: '#fff',
+
+          '&[disabled]': {
+            opacity: .3,
+            color: '#000',
+          }
+        }
+    }
+  }
+)
 
 export default Button
