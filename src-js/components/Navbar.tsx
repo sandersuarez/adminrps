c2398ce5fb4d6d888dbd037228e27c20bd23ca87
sandersuarez <sandersuarezlara@gmail.com>
2022-06-14 20:49:23 +0200

    Add the Navbar layout styles

--- src-js/components/Navbar.tsx
+++ src-js/components/Navbar.tsx
@@ -1,21 +1,35 @@
 import React, { FC } from 'react'
 import Sections from '../shapes/Sections'
 import styled from '@emotion/styled'
-import fonts from '../styles/fonts'
-
-const NavbarStyles = styled.nav(
-  fonts.body
-)
+import colors from '../styles/colors'
+import breakpoints from '../styles/breakpoints'
 
 interface IProps {
   selected: Sections
 }
 
+const NavBarStyles = styled.nav(
+  {
+    bottom: 0,
+    left: 0,
+    right: 0,
+    position: 'fixed',
+    backgroundColor: colors.menu,
+    height: '6.5rem',
+    zIndex: 1,
+    [breakpoints.desktop]: {
+      right: 'auto',
+      width: '6.5rem',
+      height: 'calc(100% - 3.5rem)',
+    }
+  },
+)
+
 const Navbar: FC<IProps> = () => {
   return (
-    <NavbarStyles>
-      <p>Holi</p>
-    </NavbarStyles>
+    <NavBarStyles>
+      <p>Menu</p>
+    </NavBarStyles>
   )
 }
 
