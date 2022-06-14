5e2c11edfa56e84d3ab570225358fda26c2c2781
sandersuarez <sandersuarezlara@gmail.com>
2022-06-14 20:42:47 +0200

    Change the fonts' module to not store classnames but objects

--- src-js/styles/fonts.ts
+++ src-js/styles/fonts.ts
@@ -1,38 +1,37 @@
-import { css } from '@emotion/css'
 import breakpoints from './breakpoints'
 
 const fonts = {
-  titleBar: css({
+  titleBar: {
     fontSize: '1.6rem',
     color: '#FFFFFF',
     [breakpoints.tablet]: {
       fontSize: '1.9rem',
     }
-  }),
-  body: css({
+  },
+  body: {
     fontSize: '1.6rem',
     [breakpoints.tablet]: {
       fontSize: '1.9rem',
     }
-  }),
-  title: css({
+  },
+  title: {
     fontSize: '1.8rem',
     [breakpoints.tablet]: {
       fontSize: '2.2rem',
     }
-  }),
-  subtitle: css({
+  },
+  subtitle: {
     fontSize: '1.8rem',
     [breakpoints.tablet]: {
       fontSize: '2rem',
     }
-  }),
-  orderNumber: css({
+  },
+  orderNumber: {
     fontSize: '2.2rem',
     [breakpoints.tablet]: {
       fontSize: '3.2rem',
     }
-  })
+  }
 }
 
 export default fonts
