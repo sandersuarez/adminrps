bbdcc7c26ee5029fe383913d178ff03b0cfcc51f
sandersuarez <sandersuarezlara@gmail.com>
2022-06-16 19:07:07 +0200

    Add the welcome layer and the active orders component to the home

--- src-js/components/Home.tsx
+++ src-js/components/Home.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import Button from './Button'
+import WelcomeLayer from './WelcomeLayer'
+import styled from '@emotion/styled'
+import breakpoints from '../styles/breakpoints'
+import ActiveOrders from './ActiveOrders'
+
+const Container = styled.section(
+  {
+    'button.new-order-button': {
+      position: 'fixed',
+      bottom: '7.5rem',
+      right: '2rem',
+      [breakpoints.tablet]: {
+        bottom: '8.5rem',
+        right: '3rem',
+      },
+      [breakpoints.desktop]: {
+        bottom: '2rem',
+      },
+    },
+
+  },
+)
 
 const Home = () => {
   return (
-    <section>
-      <p>Home</p>
-      <Button customType='danger'>Hello</Button>
-    </section>
+    <Container>
+      <WelcomeLayer userName={ 'sandy' } />
+      <Button className='new-order-button' customType={ 'secondary' }>Nuevo pedido</Button>
+      <ActiveOrders />
+    </Container>
   )
 }
 
