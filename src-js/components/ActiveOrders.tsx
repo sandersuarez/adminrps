529f25ee98b6d2ea8d08d1c66b92f1068a28bf3a
sandersuarez <sandersuarezlara@gmail.com>
2022-06-16 19:05:33 +0200

    Add the note container, the alert, and the search bar to the active orders component

--- src-js/components/ActiveOrders.tsx
+++ src-js/components/ActiveOrders.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import styled from '@emotion/styled'
+import breakpoints from '../styles/breakpoints'
+import SearchBar from './SearchBar'
+import NoteContainer from './NoteContainer'
+import Alert from './Alert'
+
+const Container = styled.article(
+  {
+    margin: '1rem 0 0 0',
+    [breakpoints.tablet]: {
+      margin: '2rem 0 0 0',
+    },
+  },
+)
 
 const ActiveOrders = () => {
   return (
-    <article>
+    <Container>
       <h2>Pedidos en elaboración</h2>
-    </article>
+      <SearchBar />
+      <Alert message={ 'error' } type={ 'error' } />
+      <NoteContainer />
+    </Container>
   )
 }
 
