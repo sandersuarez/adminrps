3280bcd97a342a67363dfdcbab748e666117cf6c
sandersuarez <sandersuarezlara@gmail.com>
2022-06-16 16:36:26 +0200

    Add the small tablet breakpoint

--- src-js/styles/breakpoints.ts
+++ src-js/styles/breakpoints.ts
@@ -1,4 +1,5 @@
 const breakpoints = {
+  smallTablet: '@media only screen and (min-width: 500px)',
   tablet: '@media only screen and (min-width: 700px)',
   desktop: '@media only screen and (min-width: 992px)'
 }
