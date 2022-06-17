8ab5c316b1cf2ff62b43c2fe92f7f2bbcc26a211
sandersuarez <sandersuarezlara@gmail.com>
2022-06-17 16:13:12 +0200

    Add the small and big desktop breakpoints

--- src-js/styles/breakpoints.ts
+++ src-js/styles/breakpoints.ts
@@ -1,7 +1,9 @@
 const breakpoints = {
   smallTablet: '@media only screen and (min-width: 500px)',
   tablet: '@media only screen and (min-width: 700px)',
-  desktop: '@media only screen and (min-width: 992px)'
+  smallDesktop: '@media only screen and (min-width: 800px)',
+  desktop: '@media only screen and (min-width: 992px)',
+  bigDesktop: '@media only screen and (min-width: 1400px)',
 }
 
 export default breakpoints
