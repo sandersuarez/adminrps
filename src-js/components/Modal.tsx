3f7254247315999bf65ea5fda62675a3b514349f
Fabian Andres Franco Lozada <lozandyloz@icloud.com>
2022-06-20 17:37:00 +0200

    Modal done

--- src-js/components/Modal.tsx
+++ src-js/components/Modal.tsx
@@ -6,11 +6,15 @@ import { ButtonProps } from './Button'
 const Container = styled.article({ background: colors.section })
 
 const TopBar = styled.div({
+  position: 'relative',
   height: '3.3rem',
   background: colors.primary,
 })
 
 const CloseButton = styled.button({
+  position: 'absolute',
+  top: '0',
+  right: 0,
   width: '3.3rem',
   height: '3.3rem',
   display: 'flex',
@@ -21,17 +25,37 @@ const CloseButton = styled.button({
   },
 })
 
+const P = styled.p({
+  margin: '1.5rem 0 1.8rem',
+})
+
+const ButtonsContainer = styled.div({
+  display: 'flex',
+  justifyContent: 'center',
+  gap: '1rem',
+  marginBottom: '1rem',
+})
+
 export interface AlertProps {
-  onClose: () => void
+  onCloseClick: () => void
   message: string
   leftButton: ReactElement<ButtonProps>
   rightButton: ReactElement<ButtonProps>
 }
 
-const Modal: FC<AlertProps> = () => {
+const Modal: FC<AlertProps> = (
+  { onCloseClick, message, leftButton, rightButton },
+) => {
   return (
     <Container>
-
+      <TopBar>
+        <CloseButton onClick={ onCloseClick } />
+      </TopBar>
+      <P>{ message }</P>
+      <ButtonsContainer>
+        { leftButton }
+        { rightButton }
+      </ButtonsContainer>
     </Container>
   )
 }
