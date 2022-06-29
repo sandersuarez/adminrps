074118e6c5cddef1d0d1c5501fcca96d8cf69fbc
sandersuarez <sandersuarezlara@gmail.com>
2022-06-29 20:40:11 +0200

    Complete the pagination component

--- src-js/components/Pagination.tsx
+++ src-js/components/Pagination.tsx
@@ -1,98 +1,185 @@
-import React from 'react'
+import React, { FC, ReactElement } from 'react'
 import styled from '@emotion/styled'
-import Button from './Button'
 import colors from '../styles/colors'
 import IconUpDown from './svg/IconUpDown'
+import breakpoints from '../styles/breakpoints'
+import { css } from '@emotion/react'
 
-const Wrapper = styled.div({
-  display: 'flex',
-  justifyContent: 'center',
-})
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
 
-const Container = styled.div({
-  display: 'flex',
-  flexFlow: 'row wrap',
-  flexBasis: '28rem',
-  margin: '1rem 0 0 0',
-  p: {
-    margin: '0 0 1rem 0',
-    fontWeight: 'bold',
-  },
-})
+const Container = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  flex-basis: 28rem;
+  margin: 2rem 0 0 0;
 
-const Controls = styled.div({
-  display: 'flex',
-  flexBasis: '28rem',
-  alignSelf: 'center',
-  height: '3.5rem',
-  button: {
-    flexShrink: 0,
-  },
-})
+  p {
+    margin: 0 0 1.5rem 0;
+    font-weight: bold;
+  }
 
-const PageNumber = styled.div({
-  display: 'flex',
-  flexGrow: 1,
-  maxWidth: '10rem',
-  background: colors.section,
+  ${ breakpoints.tablet } {
+    flex-basis: 46rem;
+  }
+`
+
+const Controls = styled.div`
+  display: flex;
+  flex-basis: 28rem;
+  align-self: center;
+  justify-content: stretch;
+  height: 3.5rem;
+
+  button {
+    flex-shrink: 0;
+    padding: 0 0.9rem;
+  }
+
+  ${ breakpoints.tablet } {
+    height: 4.5rem;
+    flex-basis: 46rem;
+
+    button {
+      padding: 0 2rem;
+    }
+  }
+`
+
+const PageNumber = styled.div`
+  display: flex;
+  flex-grow: 1;
+  background: ${ colors.section };
 
   // reset input
-  input: {
-    width: '100%',
-    appearance: 'none',
-    font: 'inherit',
-    border: 'none',
-    background: 'transparent',
-    textAlign: 'center',
-    '&:focus': {
-      outline: 'none',
-      boxShadow: 'none',
-    },
-    '&[type=number]::-webkit-inner-spin-button, &[type=number]::-webkit-outer-spin-button,': {
-      appearance: 'none',
-      margin: 0,
-    },
-  },
-})
+  input {
+    width: 100%;
+    appearance: none;
+    font: inherit;
+    border: none;
+    background: transparent;
+    text-align: center;
 
-const PaginationButton = styled(Button)`
+    &:focus {
+      outline: none;
+      box-shadow: none;
+    }
+
+    &[type=number]::-webkit-inner-spin-button, &[type=number]::-webkit-outer-spin-button {
+      appearance: none;
+      margin: 0;
+    }
+  }
+`
+
+const buttonStyles = css`
+  // reset styles
+  cursor: pointer;
+  border: none;
+  font: inherit;
+  opacity: 1;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: ${ colors.section };
   border-radius: 0;
   padding: 0;
   height: unset;
 `
 
-const FirstPageButton = styled(PaginationButton)({
-  width: '5rem',
-  margin: '0 1rem 0 0',
-  svg: {
-    transform: 'rotate(270deg)',
-  },
-})
+const PaginationButton: FC<{ className?: string, children?: ReactElement[] }> = ({ className, children }) => (
+  <button className={ className } css={ buttonStyles }>
+    { children }
+  </button>
+)
 
-const PreviousPageButton = styled(PaginationButton)({
-  width: '3.5rem',
-  margin: '0 1rem 0 0',
-  svg: {
-    transform: 'rotate(270deg)',
-  },
-})
+const FirstPageButtonStyles = css`
+  width: 5rem;
+  margin: 0 1rem 0 0;
+  border-radius: 2.3rem 0 0 2.3rem;
 
-const NextPageButton = styled(PaginationButton)({
-  width: '3.5rem',
-  margin: '0 0 0 1rem',
-  svg: {
-    transform: 'rotate(90deg)',
-  },
-})
+  svg {
+    transform: rotate(270deg);
 
-const LastPageButton = styled(PaginationButton)({
-  width: '5rem',
-  margin: '0 0 0 1rem',
-  svg: {
-    transform: 'rotate(90deg)',
-  },
-})
+    &:nth-of-type(1) {
+      margin: 0 -0.3rem 0 0.3rem;
+    }
+
+    &:nth-of-type(2) {
+      margin: 0 0.3rem 0 -0.3rem;
+    }
+  }
+
+  ${ breakpoints.tablet } {
+    margin: 0 1.5rem 0 0;
+    width: 8.2rem;
+
+    svg {
+      height: 0.88rem;
+    }
+  }
+`
+
+const PreviousPageButtonStyles = css`
+  width: 3.5rem;
+  margin: 0 1rem 0 0;
+
+  svg {
+    transform: rotate(270deg);
+    margin-right: 0.2rem;
+  }
+
+  ${ breakpoints.tablet } {
+    margin: 0 1.5rem 0 0;
+    width: 5.8rem;
+  }
+`
+
+const NextPageButtonStyles = css`
+  width: 3.5rem;
+  margin: 0 0 0 1rem;
+
+  svg {
+    transform: rotate(90deg);
+    margin-left: 0.2rem;
+  }
+
+  ${ breakpoints.tablet } {
+    margin: 0 0 0 1.5rem;
+    width: 5.8rem;
+  }
+`
+
+const LastPageButtonStyles = css`
+  width: 5rem;
+  margin: 0 0 0 1rem;
+  border-radius: 0 2.3rem 2.3rem 0;
+
+  svg {
+    transform: rotate(90deg);
+
+    &:nth-of-type(1) {
+      margin: 0 -0.3rem 0 0.3rem;
+    }
+  ;
+
+    &:nth-of-type(2) {
+      margin: 0 0.3rem 0 -0.3rem;
+    }
+  }
+
+  ${ breakpoints.tablet } {
+    margin: 0 0 0 1.5rem;
+    width: 8.2rem;
+
+    svg {
+      height: 0.88rem;
+    }
+  }
+`
 
 const Pagination = () => {
   return (
@@ -100,23 +187,23 @@ const Pagination = () => {
       <Container>
         <p>X páginas de resultados</p>
         <Controls>
-          <FirstPageButton customType={ 'paging' }>
+          <PaginationButton css={ FirstPageButtonStyles }>
             <IconUpDown />
             <IconUpDown />
-          </FirstPageButton>
-          <PreviousPageButton customType={ 'paging' }>
+          </PaginationButton>
+          <PaginationButton css={ PreviousPageButtonStyles }>
             <IconUpDown />
-          </PreviousPageButton>
+          </PaginationButton>
           <PageNumber>
-            <input type={ 'number' } value={ 1 } />
+            <input type={ 'number' } min={ 1 } defaultValue={ 1 } />
           </PageNumber>
-          <NextPageButton customType={ 'paging' }>
+          <PaginationButton css={ NextPageButtonStyles }>
             <IconUpDown />
-          </NextPageButton>
-          <LastPageButton customType={ 'paging' }>
+          </PaginationButton>
+          <PaginationButton css={ LastPageButtonStyles }>
             <IconUpDown />
             <IconUpDown />
-          </LastPageButton>
+          </PaginationButton>
         </Controls>
       </Container>
     </Wrapper>
