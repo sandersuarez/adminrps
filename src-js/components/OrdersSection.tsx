d484b95bf3d5deb3dd8ce6a80a5c11bc6bc542ba
sandersuarez <sandersuarezlara@gmail.com>
2022-07-11 17:42:39 +0200

    Add the section buttons in the orders section

--- src-js/components/OrdersSection.tsx
+++ src-js/components/OrdersSection.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import SectionButtonsWrapper from './SectionButtonsWrapper'
+import SectionButton from './SectionButton'
+import IconOrder from './svg/IconOrder'
+import IconPencil from './svg/IconPencil'
+import IconArchive from './svg/IconArchive'
+import IconBrokenClock from './svg/IconBrokenClock'
 
 const OrdersSection = () => {
   return (
     <section>
-      <p>Orders Section</p>
+      <SectionButtonsWrapper>
+        <SectionButton title={ 'Pedidos en Elaboración' } icon={ <IconOrder /> } />
+        <SectionButton title={ 'Borradores' } icon={ <IconPencil /> } />
+        <SectionButton title={ 'Pedidos vendidos' } icon={ <IconArchive /> } />
+        <SectionButton title={ 'Pedidos sin reclamar' } icon={ <IconBrokenClock /> } />
+      </SectionButtonsWrapper>
     </section>
   )
 }
