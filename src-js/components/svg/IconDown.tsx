f6cde6398dabc957cffa873ce5d89f0df711d7b6
sandersuarez <sandersuarezlara@gmail.com>
2022-08-12 14:13:24 +0200

    Add the down arrow icon

---
+++ src-js/components/svg/IconDown.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+
+const SvgDown = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    viewBox='0 0 11 7'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M8.89504 1.12874L5.50004 4.52374L2.10504 1.12874C1.76379 0.78749 1.21254 0.78749 0.871294 1.12874C0.530044 1.46999 0.530044 2.02124 0.871294 2.36249L4.88754 6.37874C5.22879 6.71999 5.78004 6.71999 6.12129 6.37874L10.1375 2.36249C10.4788 2.02124 10.4788 1.46999 10.1375 1.12874C9.79629 0.79624 9.23629 0.78749 8.89504 1.12874V1.12874Z'
+      color='#000'
+      style={{
+        fontFeatureSettings: 'normal',
+        fontVariantAlternates: 'normal',
+        fontVariantCaps: 'normal',
+        fontVariantEastAsian: 'normal',
+        fontVariantLigatures: 'normal',
+        fontVariantNumeric: 'normal',
+        fontVariantPosition: 'normal',
+        fontVariationSettings: 'normal',
+        inlineSize: 0,
+        isolation: 'auto',
+        mixBlendMode: 'normal',
+        shapeMargin: 0,
+        textDecorationColor: '#000',
+        textDecorationLine: 'none',
+        textDecorationStyle: 'solid',
+        textIndent: 0,
+        textOrientation: 'mixed',
+        textTransform: 'none',
+        whiteSpace: 'normal',
+      }}
+    />
+  </svg>
+);
+
+export default SvgDown;
