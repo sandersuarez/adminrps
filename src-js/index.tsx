83fe1fc3cde0e8608ba89a979655f7d70461ed38
sandersuarez <sandersuarezlara@gmail.com>
2022-08-13 18:50:18 +0200

    Add the bootstrap icons

--- src-js/index.tsx
+++ src-js/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import 'modern-normalize/modern-normalize.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './styles/global.css'
 
 const container = document.getElementById('root')
