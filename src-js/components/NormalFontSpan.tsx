0db018ddddee736b0bcdb1fe0ece562e5f53714b
sandersuarez <sandersuarezlara@gmail.com>
2023-02-21 22:26:37 +0100

    Add the normal font span utility component

---
+++ src-js/components/NormalFontSpan.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const NormalFontSpan = styled.span`
+  display: block;
+  margin-top: .3em;
+  font-weight: normal;
+`
+
+export default NormalFontSpan
